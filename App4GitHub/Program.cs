22c0df84af141ad02c1fcffc4fc0fc7e43ac6fd8
prasahanpatki26 <iamprajwal9@gmail.com>
2022-08-11 09:32:13 +0530

    Add project files.

---
+++ App4GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App4GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
